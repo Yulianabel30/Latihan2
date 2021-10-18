f5a734dedc4cb403b0dbb9b961742b9bd20b3613
Yulianabel30 <yulianabella914@gmail.com>
2021-10-18 13:34:54 +0700

    LATIHAN PERTEMUAN 2

---
+++ contoh.php
@@ -0,0 +1,12 @@
+<?php
+class contoh extends CI_Controller
+{
+        public function index()
+        {
+            echo "<h1>Perkenalkan</>";
+            echo"Nama saya Yuliana Bella
+                Saya tinggal di daerah Tasikmalaya
+                olah raga yang saya sukai adalah
+                Pencak silat";
+        }
+    }
